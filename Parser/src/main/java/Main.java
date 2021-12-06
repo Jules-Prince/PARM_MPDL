30313eb2c2723ee2ff2e775a17aa637b24954618
Jules Prince <jules.prince.fr@icloud.com>
2021-12-06 15:33:41 +0100

    First commit of Parser

---
+++ Parser/src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //String input = args[1];
+        //String output = args[2];
+
+        System.out.println("Test");
+    }
+}
